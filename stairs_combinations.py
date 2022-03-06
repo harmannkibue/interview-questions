9347bc6939b87ad589c1089ac4212ef071c58d94
harmannkibue <harmannkibue@gmail.com>
2022-03-06 10:13:38 +0300

    added the docs on the stairs combinations challenge

--- stairs_combinations.py
+++ stairs_combinations.py
@@ -1,9 +1,17 @@
-def stairs_combination(n):
+def stairs_combination(n) -> int:
+    """
+    In a stairs you are allowed to take one,two,three or their combinations
+    The function bellow allows to calculate how many possible ways of combinations
+    one can make given n the number of stairs
+    :param integer n:
+    :return integer the number of combinations:
+    """
     if n == 0:
         return 1
     elif n < 0:
         return 0
     else:
-        return stairs_combination(n-1) + stairs_combination(n-2) + stairs_combination(n-3)
+        return stairs_combination(n - 1) + stairs_combination(n - 2) + stairs_combination(n - 3)
+
 
 print(stairs_combination(4))
